dcf4c4d80de09f5560029b9d547397769c03d26d
JoelFong85 <36076912+JoelFong85@users.noreply.github.com>
2018-10-10 23:29:17 +0800

    added toString / string.Format

---
+++ ShamHmWk/ToStringStringFormat.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShamHmWk
+{
+    public class Person
+    {
+        public string name { get; set; }
+        public string age { get; set; }
+
+        public Person(string name, string age)
+        {
+            this.name = name;
+            this.age = age;
+        }
+
+        public override string ToString()
+        {
+            return "Person: " + name + " " + age;
+        }
+    }
+
+
+
+    class ToStringStringFormat
+    {
+
+        static void Main(string[] args)
+        {
+            Person p = new Person("Sham", "18");
+            Console.WriteLine(p.ToString());
+            Console.WriteLine(String.Format("Person is {0}", p));
+            Console.WriteLine(String.Format("Person's name is {0} and her age is {1}", p.name, p.age));
+
+        }
+
+
+
+    }
+}
