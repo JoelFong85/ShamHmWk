b1407f741eeed0db91da2d128c4f7eade62902d0
JoelFong85 <36076912+JoelFong85@users.noreply.github.com>
2018-11-04 22:46:19 +0800

    adding testIfSomething

---
+++ ShamHmWk/testIfSomething.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShamHmWk
+{
+    class testIfSomething
+    {
+        static void Main(string[] args)
+        {
+            bool failed = true;
+
+            if (failed)
+            {
+                Console.WriteLine(failed.ToString());
+            }
+
+        }
+    }
+}
